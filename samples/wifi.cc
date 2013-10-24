fdd1dc662a8ab67ff294453ee72e2d5d216a0748
Sebastian <sebastian.krahmer@gmail.com>
2013-10-24 11:15:15 +0200

    - adding wifi example

---
+++ samples/wifi.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <usi++/usi++.h>
+
+using namespace std;
+using namespace usipp;
+
+int main()
+{
+	usipp::pcap wmon;
+
+	if (wmon.init_device("mon0", 1, 1500) < 0) {
+		cerr<<wmon.why()<<endl;
+		return 1;
+	}
+
+	string pkt = "", s = "";
+	for (;;) {
+		cerr<<"pkt: "<<wmon.sniffpack(pkt).size()<<endl;
+		cerr<<"cooked hdr: "<<wmon.get_cooked(s).size()<<endl
+		    <<"frame: "<<wmon.get_frame(s).size()<<endl;
+	}
+
+	return 0;
+}
+
+
+
