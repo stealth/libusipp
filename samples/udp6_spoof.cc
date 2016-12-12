c2a443c9c24d463e2af97f5a285d624a50b4025d
Sebastian <sebastian.krahmer@gmail.com>
2016-12-12 15:01:33 +0100

    - libusipp: adding udp6 spoofing example for osx

--- samples/udp6_spoof.cc
+++ samples/udp6_spoof.cc
@@ -12,15 +12,31 @@ using namespace usipp;
 int main(int argc, char **argv)
 {
 
-	if (argc < 2) {
-		cout<<argv[0]<<" [src] [dst]\n";
+	if (argc < 3) {
+		cout<<argv[0]<<" <src> <dst> [dev]\n";
 		exit(1);
 	}
+
 	UDP6 udp(argv[2]);
-        udp.set_srcport(1);
+
+	if (argc == 4) {
+		usipp::pcap *rx = reinterpret_cast<usipp::pcap *>(udp.raw_rx());
+		if (rx->init_device(argv[3], 1, usipp::max_packet_size) < 0) {
+			cerr<<udp.why()<<endl;
+			return 1;
+		}
+		TX_pcap_eth *tx = new TX_pcap_eth(rx);
+		tx->set_l2src("00:22:33:44:55:66");
+		tx->set_l2dst("00:11:33:44:55:66");
+		tx->set_type(numbers::eth_p_ipv6);
+		udp.register_tx(tx);
+	}
+
+	udp.set_srcport(1);
+	udp.set_proto(17);
 	udp.set_dstport(111);
 	udp.set_src(argv[1]);
-       	udp.sendpack("\xff\xff");
+	udp.sendpack("\xff\xff");
 
         return 0;
 }
